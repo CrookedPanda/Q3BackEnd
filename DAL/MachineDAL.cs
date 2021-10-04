988902231e77a9edeeadcd25c35f1e17701aeca0
Jaronpoel <jaron.poel@gmail.com>
2021-10-04 15:29:52 +0200

    entity for component and machine

---
+++ DAL/MachineDAL.cs
@@ -0,0 +1,34 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public class MachineDAL
+    {
+        public static void Main() {
+        
+            using (var db = new ApplicationDataContext()){
+                
+                // Create
+                db.Add(new ComponentDTO {});
+                db.SaveChanges();
+
+                // Read
+                var component = db.DATABASENAAM
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                component.Url = "https://devblogs.microsoft.com/dotnet";
+                component.ActionCount.Add(34873);
+                db.SaveChanges();
+
+                // Delete
+                db.Remove(component);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
