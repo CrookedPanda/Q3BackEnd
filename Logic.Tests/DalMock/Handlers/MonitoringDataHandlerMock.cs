a56942dc2d2fe4dac22120cc8b11f341b762b055
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-11-30 16:04:23 +0100

    Add tests for monitoring data

--- Logic.Tests/DalMock/Handlers/MonitoringDataHandlerMock.cs
+++ Logic.Tests/DalMock/Handlers/MonitoringDataHandlerMock.cs
@@ -6,7 +6,16 @@ using System.Linq;
 namespace Logic.Tests.DalMock.Handlers;
 
 internal class MonitoringDataHandlerMock : Imonitoring_dataHandler {
-    private readonly List<monitoring_dataDTO> MonitoringData = new List<monitoring_dataDTO>();
+    private readonly List<monitoring_dataDTO> MonitoringData = new List<monitoring_dataDTO>() {
+        new monitoring_dataDTO() { id = 1, board = 1, port = 1 },
+        new monitoring_dataDTO() { id = 2, board = 1, port = 2 },
+        new monitoring_dataDTO() { id = 3, board = 1, port = 3 },
+        new monitoring_dataDTO() { id = 4, board = 1, port = 4 },
+        new monitoring_dataDTO() { id = 5, board = 2, port = 1 },
+        new monitoring_dataDTO() { id = 6, board = 2, port = 2 },
+        new monitoring_dataDTO() { id = 7, board = 3, port = 1 },
+        new monitoring_dataDTO() { id = 8, board = 3, port = 3 }
+    };
 
     public IEnumerable<monitoring_dataDTO> Get() {
         return MonitoringData;
