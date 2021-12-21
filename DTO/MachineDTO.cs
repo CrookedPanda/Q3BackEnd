91bfa3890b0f2e2d448d1724cdf32dc11af6c81d
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-12-21 12:00:22 +0100

    Update machine history to new format

--- DTO/MachineDTO.cs
+++ DTO/MachineDTO.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace DTO
-{
-    public class MachineDTO
-    {
-        public MachineDTO(string nm, List<string> components, List<UptimeDTO> uptime)
-        {
-            name = nm;
+namespace DTO {
+    public class MachineDTO {
+        public MachineDTO(string name, List<string> components, IEnumerable<UptimeDTO> data) {
+            this.name = name;
             this.components = components;
-            this.uptime = uptime;
+            this.data = data;
         }
         public string name { get; set; }
         public List<string> components { get; set; }
-        public List<UptimeDTO> uptime { get; set; }
+        public IEnumerable<UptimeDTO> data { get; set; }
     }
 }
